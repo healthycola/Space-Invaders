1a4c5189346bee6cd85733999624db8048e606bd
Aamir Jawaid <healthycola@gmail.com>
2013-02-17 00:51:56 -0800

    Initial Commit

---
+++ Game101/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Game101
+//
+//  Created by Aamir Jawaid on 2013-02-17.
+//  Copyright (c) 2013 Aamir Jawaid. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
